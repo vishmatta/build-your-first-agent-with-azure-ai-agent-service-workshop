d5e82c66e9ab0179d7535badc04a34c8f3fa36ad
vishmatta <65240766+vishmatta@users.noreply.github.com>
2025-04-21 02:30:30 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   infra/output.json
    	modified:   src/python/workshop/main.py

--- src/python/workshop/main.py
+++ src/python/workshop/main.py
@@ -56,9 +56,9 @@ functions = AsyncFunctionTool(
     }
 )
 
-# INSTRUCTIONS_FILE = "instructions/function_calling.txt"
-# INSTRUCTIONS_FILE = "instructions/file_search.txt"
-# INSTRUCTIONS_FILE = "instructions/code_interpreter.txt"
+INSTRUCTIONS_FILE = "instructions/function_calling.txt"
+INSTRUCTIONS_FILE = "instructions/file_search.txt"
+INSTRUCTIONS_FILE = "instructions/code_interpreter.txt"
 # INSTRUCTIONS_FILE = "instructions/code_interpreter_multilingual.txt"
 # INSTRUCTIONS_FILE = "instructions/bing_grounding.txt"
 
@@ -68,20 +68,20 @@ async def add_agent_tools() -> None:
     font_file_info = None
 
     # Add the functions tool
-    # toolset.add(functions)
+    toolset.add(functions)
 
     # Add the tents data sheet to a new vector data store
-    # vector_store = await utilities.create_vector_store(
-    #     project_client,
-    #     files=[TENTS_DATA_SHEET_FILE],
-    #     vector_store_name="Contoso Product Information Vector Store",
-    # )
-    # file_search_tool = FileSearchTool(vector_store_ids=[vector_store.id])
-    # toolset.add(file_search_tool)
+    vector_store = await utilities.create_vector_store(
+        project_client,
+        files=[TENTS_DATA_SHEET_FILE],
+        vector_store_name="Contoso Product Information Vector Store",
+    )
+    file_search_tool = FileSearchTool(vector_store_ids=[vector_store.id])
+    toolset.add(file_search_tool)
 
     # Add the code interpreter tool
-    # code_interpreter = CodeInterpreterTool()
-    # toolset.add(code_interpreter)
+    code_interpreter = CodeInterpreterTool()
+    toolset.add(code_interpreter)
 
     # Add multilingual support to the code interpreter
     # font_file_info = await utilities.upload_file(project_client, utilities.shared_files_path / FONTS_ZIP)
